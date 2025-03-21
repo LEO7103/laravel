7938d12a8ab205d7d38c308778e906abbc93ce3d
Leo7103 <asdx165@yahoo.com.tw>
2025-03-21 23:57:13 +0800

    Initial commit

---
+++ config/order_status.php
@@ -0,0 +1,55 @@
+<?php
+return [
+    'order_status_admin' => [
+        'pending' => [
+            'status' => 'Pending',
+            'detail' => '你的訂單正在審核'
+        ],
+        'Processed_and_ready_to_ship' => [
+            'status' => 'Processed and ready to ship',
+            'detail' => '你的包裹已經在運送至轉倉庫'
+        ],
+        'dropped_off' => [
+            'status' => 'Dropped_off',
+            'detail' => '你的包裹已遺失包裹已遺失'
+        ],
+        'shipped' => [
+            'status' => 'Shipped',
+            'detail' => '你的訂單正已抵達倉庫'
+        ],
+        'out_for_delivery' => [
+            'status' => 'Out For Felivery',
+            'detail' => '你的訂單正在出貨'
+        ],
+        'deliverd' => [
+            'status' => 'Deliverd',
+            'detail' => '你的訂單已交付'
+        ],
+        'canceled' => [
+            'status' => 'Canceld',
+            'detail' => '你的訂單已交付'
+        ]
+    ],
+
+
+    'order_status_vendor' => [
+        [
+            'status' => 'Pending',
+            'detail' => '你的訂單正在審核'
+        ],
+        'Processed_and_ready_to_ship' => [
+            'status' => 'Processed and ready to ship',
+            'detail' => '你的包裹已經在運送至轉倉庫'
+        ]
+
+    ],
+
+
+
+
+
+
+
+
+
+];
